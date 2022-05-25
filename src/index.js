d5639e72ecb05dcc12904b517be1a4453e8672f1
Flolmv <felixmv@web.de>
2022-05-25 15:51:45 +0200

    setting up basic communication

--- src/index.js
+++ src/index.js
@@ -2,3 +2,11 @@ const express = require("express");
 const app = express();
 app.listen(3000, () => console.log("listening"));
 app.use(express.static("public"));
+app.use(express.json() );
+
+app.post("/api", (request, response)=> {
+  console.log(request.body);
+  response.json({
+    status: "sucess"
+  })
+})
