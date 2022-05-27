336f25e810402953f8d7c2d731e739dfae397200
Flolmv <felixmv@web.de>
2022-05-27 19:13:28 +0200

    fixing basic communication between website and server

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ app.listen(3000, () => console.log("listening"));
 app.use(express.static("public"));
 app.use(express.json() );
 
+let inputData = {};
 app.post("/api", (request, response)=> {
-  console.log(request.body);
+  inputData = request.body;
+  console.log(inputData);
   response.json({
     status: "sucess"
   })
