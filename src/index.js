d2b660738b0ba8bfdc4e35b562941c83e1091a1c
Flolmv <felixmv@web.de>
2022-06-01 10:16:26 +0200

    deleting computation section

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const fs = require("fs");
 app.listen(3000, () => console.log("listening"));
 app.use(express.static("public"));
 app.use(express.json() );
@@ -7,6 +8,8 @@ app.use(express.json() );
 let inputData = {};
 app.post("/api", (request, response)=> {
   inputData = request.body;
+
+
   console.log(inputData);
   response.json({
     status: "sucess"
